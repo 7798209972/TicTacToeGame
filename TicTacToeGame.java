7bf3a787485a1d056a981def2f894e67d2071d64
7798209972 <patlesachin1@gmail.com>
2020-11-03 16:29:30 +0530

    Getting input from user

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,9 @@
+import java.util.*;
+
 public class TicTacToeGame
 {
+	Scanner me=new Scanner(System.in);
+	String user_input, computer_input;
 
 	TicTacToeGame()
 	{
@@ -21,13 +25,30 @@ public class TicTacToeGame
 
 	}
 
-	public void get_info()
+	public void get_input()
 	{
+		System.out.println("Please Enter 'O' or 'X' ");
+		user_input=me.next();
+
+		System.out.println(user_input);
+
+		if(user_input=="O")
+		{
+			computer_input="X";
+		}
+		else if(user_input=="X")
+		{
+			computer_input="O";
+		}
+		else
+		{
+			System.out.println("Invalid input... Please Enter 'O' or 'X'");
+		}
 
 	}
 	public static void main(String args[])
 	{
 		TicTacToeGame obj= new TicTacToeGame();
-		obj.empty_array();
+		obj.get_input();
 	}
 }
